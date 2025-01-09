e29a912ce5f5f89189a99f1cd3379e4658fdf8df
koti prasad <168547097+KOTI-PRASAD@users.noreply.github.com>
2025-01-09 12:15:51 +0530

    Add files via upload
    stock change

--- StockController.java
+++ StockController.java
@@ -3,6 +3,8 @@ package com.example.stockportfolio.controller;
 import com.example.stockportfolio.model.Stock;
 import com.example.stockportfolio.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,16 +20,46 @@ public class StockController {
         this.stockService = stockService;
     }
 
-    // Generate a random portfolio of 5 stocks for the user
-    @PostMapping("/generate-portfolio")
-    public List<Stock> generatePortfolio() {
-        return stockService.generatePortfolio();
+    // Add a new stock
+    @PostMapping
+    public ResponseEntity<Stock> addStock(@RequestBody Stock stock) {
+        Stock createdStock = stockService.addStock(stock);
+        return new ResponseEntity<>(createdStock, HttpStatus.CREATED);
     }
 
-    // Calculate the total portfolio value
+    // Update an existing stock
+    @PutMapping("/{id}")
+    public ResponseEntity<Stock> updateStock(@PathVariable Long id, @RequestBody Stock stock) {
+        Stock updatedStock = stockService.updateStock(id, stock);
+        if (updatedStock != null) {
+            return new ResponseEntity<>(updatedStock, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Delete a stock
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStock(@PathVariable Long id) {
+        if (stockService.deleteStock(id)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Fetch all stocks
+    @GetMapping
+    public ResponseEntity<List<Stock>> getAllStocks() {
+        List<Stock> stocks = stockService.getAllStocks();
+        return new ResponseEntity<>(stocks, HttpStatus.OK);
+    }
+
+    // Calculate the portfolio value
     @GetMapping("/portfolio-value")
-    public double getPortfolioValue() {
-        return stockService.calculatePortfolioValue();
+    public ResponseEntity<Double> getPortfolioValue() {
+        double portfolioValue = stockService.calculatePortfolioValue();
+        return new ResponseEntity<>(portfolioValue, HttpStatus.OK);
     }
 }
 
